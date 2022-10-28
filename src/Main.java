a14e96adeec2b5082348c90dc7b31e76d368910c
ChamChamin <Pochtaotmartina@mail.ru>
2022-10-28 11:01:59 +0900

    001

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
 import TreeFS.TreeFS;
-import java.io.File;
+
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        TreeFS tree = new TreeFS(new File("").getAbsolutePath());
-
-
-        System.out.println( tree.createXMLCode() );
+        
     }
 
 }
